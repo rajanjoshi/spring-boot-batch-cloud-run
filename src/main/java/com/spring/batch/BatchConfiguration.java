f0d8b976ee7839fd54bdde3f491d9b46a3e45ea5
rajanjoshi <joshirajan999@gmail.com>
2021-12-05 12:11:29 +0530

    Added step

--- src/main/java/com/spring/batch/BatchConfiguration.java
+++ src/main/java/com/spring/batch/BatchConfiguration.java
@@ -1,14 +1,16 @@
 package com.spring.batch;
 
+import java.io.IOException;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
@@ -16,9 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-import javax.sql.DataSource;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @EnableBatchProcessing
@@ -29,14 +30,20 @@ public class BatchConfiguration {
 
     @Autowired
     public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public CoffeeRepository repository;
     
     @Value("${file.input}")
     private String fileInput;
 
+    @Value("gs://spring-bucket-coffee/coffee-list.CSV")
+    private Resource gcsFile;
+
     @Bean
-    public FlatFileItemReader<Coffee> reader() {
+    public FlatFileItemReader<Coffee> reader() throws IOException{
         return new FlatFileItemReaderBuilder<Coffee>().name("coffeeItemReader")
-            .resource(new ClassPathResource(fileInput))
+            .resource(new PathResource(gcsFile.getURI()))
             .delimited()
             .names(new String[] { "brand", "origin", "characteristics" })
             .fieldSetMapper(new BeanWrapperFieldSetMapper<Coffee>() {{
@@ -51,11 +58,11 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public JdbcBatchItemWriter<Coffee> writer(DataSource dataSource) {
-        return new JdbcBatchItemWriterBuilder<Coffee>().itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-            .sql("INSERT INTO coffee (brand, origin, characteristics) VALUES (:brand, :origin, :characteristics)")
-            .dataSource(dataSource)
-            .build();
+    public ItemWriter<Coffee> writer() {
+        return new RepositoryItemWriterBuilder<Coffee>()
+                .repository(repository)
+                .methodName("save")
+                .build();
     }
 
     @Bean
@@ -69,12 +76,12 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Step step1(JdbcBatchItemWriter<Coffee> writer) {
+    public Step step1() throws IOException{
         return stepBuilderFactory.get("step1")
-            .<Coffee, Coffee> chunk(10)
+            .<Coffee, Coffee> chunk(2000)
             .reader(reader())
             .processor(processor())
-            .writer(writer)
+            .writer(writer())
             .build();
     }
 
