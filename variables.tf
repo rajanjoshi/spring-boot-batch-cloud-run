f5682e5061b95830781001da123800bce5b0c6fb
rajanjoshi <joshirajan999@gmail.com>
2021-12-12 16:37:09 +0530

    variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project" {
+  type        = string
+  description = "Google Cloud Platform Project ID"
+  default = "powerful-vine-329211"
+}
+
+variable "region" {
+  default = "us-central1"
+  type    = string
+}
