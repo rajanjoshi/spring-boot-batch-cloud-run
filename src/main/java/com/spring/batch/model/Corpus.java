56413e55ae6127ed067b429c4e7b46b04ea0f82f
rajanjoshi <joshirajan999@gmail.com>
2022-01-23 12:17:41 +0530

    Corpus.java

---
+++ src/main/java/com/spring/batch/model/Corpus.java
@@ -0,0 +1,20 @@
+package com.spring.batch.model;
+
+public class Corpus {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Corpus(String name) {
+        this.name = name;
+    }
+
+    
+}
