bbff9ac9eeb8f94768f85b4a62138a0042d7652e
rajanjoshi <joshirajan999@gmail.com>
2022-01-23 12:15:02 +0530

    BQItemProcessor.java

---
+++ src/main/java/com/spring/batch/BQItemProcessor.java
@@ -0,0 +1,20 @@
+package com.spring.batch;
+
+import com.spring.batch.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+public class BQItemProcessor implements ItemProcessor<Corpus, Coffee> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BQItemProcessor.class);
+
+    @Override
+    public Coffee process(final Corpus corpus) throws Exception {
+        Coffee transformedCorpus = new Coffee(corpus.getName().toUpperCase(),corpus.getName(),corpus.getName().toLowerCase());
+        LOGGER.info("Converting ( {} ) into ( {} )", corpus, transformedCorpus);
+
+        return transformedCorpus;
+    }
+
+}
